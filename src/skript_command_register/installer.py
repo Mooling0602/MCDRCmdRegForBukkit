10dd07306f97b1dc42c298426338ab01b4ff4dc4
木泠 <clemooling@outlook.com>
2024-12-13 07:09:04 +0000

    Update to v0.0.2 with locales fix

--- src/skript_command_register/installer.py
+++ src/skript_command_register/installer.py
@@ -13,4 +13,4 @@ def load_general_sk_script(skip_check: Optional[bool] = False):
     if psi.is_server_startup() or skip_check:
         if os.path.exists(f"{serverDir}/plugins/Skript/config.sk"):
             psi.execute(f"sk reload MCDR_Commands_general.sk")
-            psi.logger.info("on_installed")
+            psi.logger.info(tr("on_installed"))
